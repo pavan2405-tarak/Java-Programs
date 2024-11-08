40c6131bed70fb7d6f7ec44dfb26e4c9fa588e2b
pavan2405-tarak <bonthupavan532@gmail.com>
2024-11-08 13:40:37 +0530

    modified the file

--- Add2floatnumbers.java
+++ Add2floatnumbers.java
@@ -4,6 +4,6 @@ public static void main(String args[])
 {
 float value1=2.4,value2=2.6;
 Add2floatnumbers=value1+value2;
-System.out.println(Add2floatnumbers);
+System.out.println(Add2floatnumbers);//printing the values
 }
 }
